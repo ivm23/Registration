10033c2b8eb1350be2907815e7838b0996733adc
Ivanova Marina <marina200964@yandex.ru>
2018-04-11 18:03:52 +0300

    db

---
+++ DocsVision_Test-master/db.sql
@@ -0,0 +1,111 @@
+USE [master]
+GO
+
+/****** Object:  Database [RegistrationWithFolders.DB]    Script Date: 11.04.2018 18:03:22 ******/
+CREATE DATABASE [RegistrationWithFolders.DB]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'RegistrationWithFolders.DB', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\DATA\RegistrationWithFolders.DB.mdf' , SIZE = 5120KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
+ LOG ON 
+( NAME = N'RegistrationWithFolders.DB_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\DATA\RegistrationWithFolders.DB_log.ldf' , SIZE = 1024KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET COMPATIBILITY_LEVEL = 110
+GO
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [RegistrationWithFolders.DB].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET ANSI_NULL_DEFAULT OFF 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET ANSI_NULLS OFF 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET ANSI_PADDING OFF 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET ANSI_WARNINGS OFF 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET ARITHABORT OFF 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET AUTO_CLOSE OFF 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET AUTO_CREATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET AUTO_SHRINK OFF 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET AUTO_UPDATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET CURSOR_DEFAULT  GLOBAL 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET NUMERIC_ROUNDABORT OFF 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET QUOTED_IDENTIFIER OFF 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET RECURSIVE_TRIGGERS OFF 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET  DISABLE_BROKER 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET TRUSTWORTHY OFF 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET PARAMETERIZATION SIMPLE 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET HONOR_BROKER_PRIORITY OFF 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET RECOVERY FULL 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET  MULTI_USER 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET PAGE_VERIFY CHECKSUM  
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET DB_CHAINING OFF 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET TARGET_RECOVERY_TIME = 0 SECONDS 
+GO
+
+ALTER DATABASE [RegistrationWithFolders.DB] SET  READ_WRITE 
+GO
+
